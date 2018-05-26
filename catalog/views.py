bc6f6fce6e22d7f9e210be65c61373b3c2ef5e04
vitoloper <vitoloper@noreply.github.com>
2018-05-26 23:03:33 +0200

    Add catalog home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Book, BookInstance, Author, Genre
 
-# Create your views here.
+
+def index(request):
+    """
+    View function for index page site (home page).
+    """
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
+
+    # Render HTML
+    return render(request, 'index.html', context={'num_books': num_books, 'num_instances': num_instances,
+                                             'num_instances_available': num_instances_available,
+                                             'num_authors': num_authors})
