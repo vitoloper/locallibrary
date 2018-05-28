970fef4450d09061ecd3f21a9726953e163d59ef
vitoloper <vitoloper@noreply.github.com>
2018-05-28 16:34:45 +0200

    Add session page counter in home page

--- catalog/views.py
+++ catalog/views.py
@@ -13,10 +13,13 @@ def index(request):
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     # Render HTML
     return render(request, 'index.html', context={'num_books': num_books, 'num_instances': num_instances,
                                              'num_instances_available': num_instances_available,
-                                             'num_authors': num_authors})
+                                             'num_authors': num_authors, 'num_visits': num_visits})
 
 
 class BookListView(generic.ListView):
