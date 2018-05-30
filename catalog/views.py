0831130dc9212ca690eb9b0e07aeb0da25c67e54
vitoloper <vitoloper@noreply.github.com>
2018-05-30 17:27:51 +0200

    Add list all borrowed books

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
 from django.views import generic
 from django.shortcuts import render
 from .models import Book, BookInstance, Author, Genre
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 
 def index(request):
@@ -48,3 +48,13 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+class AllBorrowedBooksListView(PermissionRequiredMixin, generic.ListView):
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
+    permission_required = 'catalog.can_mark_returned'
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
