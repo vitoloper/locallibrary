2744673ad0f96e31c9689e797ef40f09b01bac74
vitoloper <vitoloper@noreply.github.com>
2018-06-03 17:52:05 +0200

    Change STATIC_ROOT in settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -117,7 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+# The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
